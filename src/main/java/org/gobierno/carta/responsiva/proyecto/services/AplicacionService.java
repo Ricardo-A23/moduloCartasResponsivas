183f0b842a4abf4d28b44abed95cbb9e092ad619
Ricardo Alvarez <mtzricardo765@gmail.com>
2025-08-06 12:52:53 -0600

    services: se crearon las clases service

--- src/main/java/org/gobierno/carta/responsiva/proyecto/services/AplicacionService.java
+++ src/main/java/org/gobierno/carta/responsiva/proyecto/services/AplicacionService.java
@@ -1,7 +1,4 @@
 package org.gobierno.carta.responsiva.proyecto.services;
 
-import org.gobierno.carta.responsiva.proyecto.entities.Aplicacion;
-import org.springframework.data.repository.CrudRepository;
-
 public class AplicacionService  {
 }
